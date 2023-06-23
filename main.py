cb57b1ae3a42dfcae7d19610e95898d1b7d6620c
Ajas Mohammed <ajasmohammed33@gmail.com>
2023-06-23 10:12:27 -0700

    created most of the ui of the dashboard. currently the icons are not showing after the compiling the .ui file to .py file. assuming that the problem is with the PySide6.

---
+++ main.py
@@ -0,0 +1,21 @@
+import sys
+
+from ui import *
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        QMainWindow.__init__(self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.show()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+
+    window = MainWindow()
+
+    window.show()
+    sys.exit(app.exec())
---
+++ resource_rc.py
@@ -0,0 +1,518 @@
+# Resource object code (Python 3)
+# Created by: object code
+# Created by: The Resource Compiler for Qt version 6.5.1
+# WARNING! All changes made in this file will be lost!
+
+from PySide6 import QtCore
+
+qt_resource_data = b"\
+\x00\x00\x01\x5c\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 width=\x2224\
+\x22 height=\x2224\x22 vi\
+ewBox=\x220 0 24 24\
+\x22 fill=\x22none\x22 st\
+roke=\x22#ffffff\x22 s\
+troke-width=\x222\x22 \
+stroke-linecap=\x22\
+round\x22 stroke-li\
+nejoin=\x22round\x22 c\
+lass=\x22feather fe\
+ather-bar-chart\x22\
+><line x1=\x2212\x22 y\
+1=\x2220\x22 x2=\x2212\x22 y\
+2=\x2210\x22></line><l\
+ine x1=\x2218\x22 y1=\x22\
+20\x22 x2=\x2218\x22 y2=\x22\
+4\x22></line><line \
+x1=\x226\x22 y1=\x2220\x22 x\
+2=\x226\x22 y2=\x2216\x22></\
+line></svg>\
+\x00\x00\x01E\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 width=\x2224\
+\x22 height=\x2224\x22 vi\
+ewBox=\x220 0 24 24\
+\x22 fill=\x22none\x22 st\
+roke=\x22#ffff\x22 str\
+oke-width=\x222\x22 st\
+roke-linecap=\x22ro\
+und\x22 stroke-line\
+join=\x22round\x22 cla\
+ss=\x22feather feat\
+her-home\x22><path \
+d=\x22M3 9l9-7 9 7v\
+11a2 2 0 0 1-2 2\
+H5a2 2 0 0 1-2-2\
+z\x22></path><polyl\
+ine points=\x229 22\
+ 9 12 15 12 15 2\
+2\x22></polyline></\
+svg>\
+\x00\x00\x01\xab\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 viewBox=\x22\
+0 0 448 512\x22><!-\
+-! Font Awesome \
+Pro 6.4.0 by @fo\
+ntawesome - http\
+s://fontawesome.\
+com License - ht\
+tps://fontawesom\
+e.com/license (C\
+ommercial Licens\
+e) Copyright 202\
+3 Fonticons, Inc\
+. --><path d=\x22M2\
+24 256A128 128 0\
+ 1 0 224 0a128 1\
+28 0 1 0 0 256zm\
+-45.7 48C79.8 30\
+4 0 383.8 0 482.\
+3C0 498.7 13.3 5\
+12 29.7 512H418.\
+3c16.4 0 29.7-13\
+.3 29.7-29.7C448\
+ 383.8 368.2 304\
+ 269.7 304H178.3\
+z\x22/></svg>\
+\x00\x00\x01\xd5\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 viewBox=\x22\
+0 0 448 512\x22><!-\
+-! Font Awesome \
+Pro 6.4.0 by @fo\
+ntawesome - http\
+s://fontawesome.\
+com License - ht\
+tps://fontawesom\
+e.com/license (C\
+ommercial Licens\
+e) Copyright 202\
+3 Fonticons, Inc\
+. --><path d=\x22M4\
+38.6 278.6c12.5-\
+12.5 12.5-32.8 0\
+-45.3l-160-160c-\
+12.5-12.5-32.8-1\
+2.5-45.3 0s-12.5\
+ 32.8 0 45.3L338\
+.8 224 32 224c-1\
+7.7 0-32 14.3-32\
+ 32s14.3 32 32 3\
+2l306.7 0L233.4 \
+393.4c-12.5 12.5\
+-12.5 32.8 0 45.\
+3s32.8 12.5 45.3\
+ 0l160-160z\x22/></\
+svg>\
+\x00\x00\x03G\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 viewBox=\x22\
+0 0 512 512\x22><!-\
+-! Font Awesome \
+Pro 6.4.0 by @fo\
+ntawesome - http\
+s://fontawesome.\
+com License - ht\
+tps://fontawesom\
+e.com/license (C\
+ommercial Licens\
+e) Copyright 202\
+3 Fonticons, Inc\
+. --><path d=\x22M3\
+84 48c8.8 0 16 7\
+.2 16 16V448c0 8\
+.8-7.2 16-16 16H\
+96c-8.8 0-16-7.2\
+-16-16V64c0-8.8 \
+7.2-16 16-16H384\
+zM96 0C60.7 0 32\
+ 28.7 32 64V448c\
+0 35.3 28.7 64 6\
+4 64H384c35.3 0 \
+64-28.7 64-64V64\
+c0-35.3-28.7-64-\
+64-64H96zM240 25\
+6a64 64 0 1 0 0-\
+128 64 64 0 1 0 \
+0 128zm-32 32c-4\
+4.2 0-80 35.8-80\
+ 80c0 8.8 7.2 16\
+ 16 16H336c8.8 0\
+ 16-7.2 16-16c0-\
+44.2-35.8-80-80-\
+80H208zM512 80c0\
+-8.8-7.2-16-16-1\
+6s-16 7.2-16 16v\
+64c0 8.8 7.2 16 \
+16 16s16-7.2 16-\
+16V80zM496 192c-\
+8.8 0-16 7.2-16 \
+16v64c0 8.8 7.2 \
+16 16 16s16-7.2 \
+16-16V208c0-8.8-\
+7.2-16-16-16zm16\
+ 144c0-8.8-7.2-1\
+6-16-16s-16 7.2-\
+16 16v64c0 8.8 7\
+.2 16 16 16s16-7\
+.2 16-16V336z\x22/>\
+</svg>\
+\x00\x00\x01e\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 width=\x2224\
+\x22 height=\x2224\x22 vi\
+ewBox=\x220 0 24 24\
+\x22 fill=\x22none\x22 st\
+roke=\x22rgb(24, 47\
+, 255)\x22 stroke-w\
+idth=\x222\x22 stroke-\
+linecap=\x22round\x22 \
+stroke-linejoin=\
+\x22round\x22 class=\x22f\
+eather feather-b\
+ar-chart\x22><line \
+x1=\x2212\x22 y1=\x2220\x22 \
+x2=\x2212\x22 y2=\x2210\x22>\
+</line><line x1=\
+\x2218\x22 y1=\x2220\x22 x2=\
+\x2218\x22 y2=\x224\x22></li\
+ne><line x1=\x226\x22 \
+y1=\x2220\x22 x2=\x226\x22 y\
+2=\x2216\x22></line></\
+svg>\
+\x00\x00\x01s\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 width=\x2224\
+\x22 height=\x2224\x22 vi\
+ewBox=\x220 0 24 24\
+\x22 fill=\x22none\x22 st\
+roke=\x22rgb(24, 47\
+, 255)\x22 stroke-w\
+idth=\x222\x22 stroke-\
+linecap=\x22round\x22 \
+stroke-linejoin=\
+\x22round\x22 class=\x22f\
+eather feather-l\
+og-out\x22><path d=\
+\x22M9 21H5a2 2 0 0\
+ 1-2-2V5a2 2 0 0\
+ 1 2-2h4\x22></path\
+><polyline point\
+s=\x2216 17 21 12 1\
+6 7\x22></polyline>\
+<line x1=\x2221\x22 y1\
+=\x2212\x22 x2=\x229\x22 y2=\
+\x2212\x22></line></sv\
+g>\
+\x00\x00\x01j\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 width=\x2224\
+\x22 height=\x2224\x22 vi\
+ewBox=\x220 0 24 24\
+\x22 fill=\x22none\x22 st\
+roke=\x22#ffffff\x22 s\
+troke-width=\x222\x22 \
+stroke-linecap=\x22\
+round\x22 stroke-li\
+nejoin=\x22round\x22 c\
+lass=\x22feather fe\
+ather-log-out\x22><\
+path d=\x22M9 21H5a\
+2 2 0 0 1-2-2V5a\
+2 2 0 0 1 2-2h4\x22\
+></path><polylin\
+e points=\x2216 17 \
+21 12 16 7\x22></po\
+lyline><line x1=\
+\x2221\x22 y1=\x2212\x22 x2=\
+\x229\x22 y2=\x2212\x22></li\
+ne></svg>\
+\x00\x00\x02\xa8\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 viewBox=\x22\
+0 0 512 512\x22><!-\
+-! Font Awesome \
+Pro 6.4.0 by @fo\
+ntawesome - http\
+s://fontawesome.\
+com License - ht\
+tps://fontawesom\
+e.com/license (C\
+ommercial Licens\
+e) Copyright 202\
+3 Fonticons, Inc\
+. --><path d=\x22M4\
+06.5 399.6C387.4\
+ 352.9 341.5 320\
+ 288 320H224c-53\
+.5 0-99.4 32.9-1\
+18.5 79.6C69.9 3\
+62.2 48 311.7 48\
+ 256C48 141.1 14\
+1.1 48 256 48s20\
+8 93.1 208 208c0\
+ 55.7-21.9 106.2\
+-57.5 143.6zm-40\
+.1 32.7C334.4 45\
+2.4 296.6 464 25\
+6 464s-78.4-11.6\
+-110.5-31.7c7.3-\
+36.7 39.7-64.3 7\
+8.5-64.3h64c38.8\
+ 0 71.2 27.6 78.\
+5 64.3zM256 512A\
+256 256 0 1 0 25\
+6 0a256 256 0 1 \
+0 0 512zm0-272a4\
+0 40 0 1 1 0-80 \
+40 40 0 1 1 0 80\
+zm-88-40a88 88 0\
+ 1 0 176 0 88 88\
+ 0 1 0 -176 0z\x22/\
+></svg>\
+\x00\x00\x02\x0f\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 viewBox=\x22\
+0 0 448 512\x22><!-\
+-! Font Awesome \
+Pro 6.4.0 by @fo\
+ntawesome - http\
+s://fontawesome.\
+com License - ht\
+tps://fontawesom\
+e.com/license (C\
+ommercial Licens\
+e) Copyright 202\
+3 Fonticons, Inc\
+. --><path d=\x22M0\
+ 96C0 78.3 14.3 \
+64 32 64H416c17.\
+7 0 32 14.3 32 3\
+2s-14.3 32-32 32\
+H32C14.3 128 0 1\
+13.7 0 96zM0 256\
+c0-17.7 14.3-32 \
+32-32H416c17.7 0\
+ 32 14.3 32 32s-\
+14.3 32-32 32H32\
+c-17.7 0-32-14.3\
+-32-32zM448 416c\
+0 17.7-14.3 32-3\
+2 32H32c-17.7 0-\
+32-14.3-32-32s14\
+.3-32 32-32H416c\
+17.7 0 32 14.3 3\
+2 32z\x22/></svg>\
+\x00\x00\x01P\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 width=\x2224\
+\x22 height=\x2224\x22 vi\
+ewBox=\x220 0 24 24\
+\x22 fill=\x22none\x22 st\
+roke=\x22rgb(24, 47\
+, 255)\x22 stroke-w\
+idth=\x222\x22 stroke-\
+linecap=\x22round\x22 \
+stroke-linejoin=\
+\x22round\x22 class=\x22f\
+eather feather-h\
+ome\x22><path d=\x22M3\
+ 9l9-7 9 7v11a2 \
+2 0 0 1-2 2H5a2 \
+2 0 0 1-2-2z\x22></\
+path><polyline p\
+oints=\x229 22 9 12\
+ 15 12 15 22\x22></\
+polyline></svg>\
+\x00\x00\x01\xaa\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 width=\x2224\
+\x22 height=\x2224\x22 vi\
+ewBox=\x220 0 24 24\
+\x22 fill=\x22none\x22 st\
+roke=\x22#ffffff\x22 s\
+troke-width=\x222\x22 \
+stroke-linecap=\x22\
+round\x22 stroke-li\
+nejoin=\x22round\x22 c\
+lass=\x22feather fe\
+ather-file-plus\x22\
+><path d=\x22M14 2H\
+6a2 2 0 0 0-2 2v\
+16a2 2 0 0 0 2 2\
+h12a2 2 0 0 0 2-\
+2V8z\x22></path><po\
+lyline points=\x221\
+4 2 14 8 20 8\x22><\
+/polyline><line \
+x1=\x2212\x22 y1=\x2218\x22 \
+x2=\x2212\x22 y2=\x2212\x22>\
+</line><line x1=\
+\x229\x22 y1=\x2215\x22 x2=\x22\
+15\x22 y2=\x2215\x22></li\
+ne></svg>\
+\x00\x00\x01\xb3\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 width=\x2224\
+\x22 height=\x2224\x22 vi\
+ewBox=\x220 0 24 24\
+\x22 fill=\x22none\x22 st\
+roke=\x22rgb(24, 47\
+, 255)\x22 stroke-w\
+idth=\x222\x22 stroke-\
+linecap=\x22round\x22 \
+stroke-linejoin=\
+\x22round\x22 class=\x22f\
+eather feather-f\
+ile-plus\x22><path \
+d=\x22M14 2H6a2 2 0\
+ 0 0-2 2v16a2 2 \
+0 0 0 2 2h12a2 2\
+ 0 0 0 2-2V8z\x22><\
+/path><polyline \
+points=\x2214 2 14 \
+8 20 8\x22></polyli\
+ne><line x1=\x2212\x22\
+ y1=\x2218\x22 x2=\x2212\x22\
+ y2=\x2212\x22></line>\
+<line x1=\x229\x22 y1=\
+\x2215\x22 x2=\x2215\x22 y2=\
+\x2215\x22></line></sv\
+g>\
+"
+
+qt_resource_name = b"\
+\x00\x05\
+\x00o\xa6S\
+\x00i\
+\x00c\x00o\x00n\x00s\
+\x00\x13\
+\x03\x9e%\xc7\
+\x00b\
+\x00a\x00r\x00-\x00c\x00h\x00a\x00r\x00t\x00-\x00w\x00h\x00i\x00t\x00e\x00.\x00s\
+\x00v\x00g\
+\x00\x0e\
+\x0c\xd8\xda\xe7\
+\x00h\
+\x00o\x00m\x00e\x00-\x00w\x00h\x00i\x00t\x00e\x00.\x00s\x00v\x00g\
+\x00\x0e\
+\x01}\xc9'\
+\x00u\
+\x00s\x00e\x00r\x00-\x00s\x00o\x00l\x00i\x00d\x00.\x00s\x00v\x00g\
+\x00\x15\
+\x0c\xb3\xa1'\
+\x00a\
+\x00r\x00r\x00o\x00w\x00-\x00r\x00i\x00g\x00h\x00t\x00-\x00s\x00o\x00l\x00i\x00d\
+\x00.\x00s\x00v\x00g\
+\x00\x18\
+\x05O\xb8\x87\
+\x00a\
+\x00d\x00d\x00r\x00e\x00s\x00s\x00-\x00b\x00o\x00o\x00k\x00-\x00r\x00e\x00g\x00u\
+\x00l\x00a\x00r\x00.\x00s\x00v\x00g\
+\x00\x12\
+\x04\x87\xf3\xa7\
+\x00b\
+\x00a\x00r\x00-\x00c\x00h\x00a\x00r\x00t\x00-\x00b\x00l\x00u\x00e\x00.\x00s\x00v\
+\x00g\
+\x00\x10\
+\x0btkg\
+\x00l\
+\x00o\x00g\x00-\x00o\x00u\x00t\x00-\x00b\x00l\x00u\x00e\x00.\x00s\x00v\x00g\
+\x00\x11\
+\x0c\xe7\xa8'\
+\x00l\
+\x00o\x00g\x00-\x00o\x00u\x00t\x00-\x00w\x00h\x00i\x00t\x00e\x00.\x00s\x00v\x00g\
+\
+\x00\x17\
+\x03WEG\
+\x00c\
+\x00i\x00r\x00c\x00l\x00e\x00-\x00u\x00s\x00e\x00r\x00-\x00r\x00e\x00g\x00u\x00l\
+\x00a\x00r\x00.\x00s\x00v\x00g\
+\x00\x0e\
+\x03\xdb\xd4'\
+\x00b\
+\x00a\x00r\x00s\x00-\x00s\x00o\x00l\x00i\x00d\x00.\x00s\x00v\x00g\
+\x00\x0d\
+\x0ds\x5cG\
+\x00h\
+\x00o\x00m\x00e\x00-\x00b\x00l\x00u\x00e\x00.\x00s\x00v\x00g\
+\x00\x13\
+\x03\x22\xd3\x87\
+\x00f\
+\x00i\x00l\x00e\x00-\x00p\x00l\x00u\x00s\x00-\x00w\x00h\x00i\x00t\x00e\x00.\x00s\
+\x00v\x00g\
+\x00\x12\
+\x06\x90<\xc7\
+\x00f\
+\x00i\x00l\x00e\x00-\x00p\x00l\x00u\x00s\x00-\x00b\x00l\x00u\x00e\x00.\x00s\x00v\
+\x00g\
+"
+
+qt_resource_struct = b"\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x01\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x0d\x00\x00\x00\x02\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00^\x00\x00\x00\x00\x00\x01\x00\x00\x02\xa9\
+\x00\x00\x01\x88\xe2U\xd2\x15\
+\x00\x00\x01\xd4\x00\x00\x00\x00\x00\x01\x00\x00\x13\xdd\
+\x00\x00\x01\x88\xe7\x873\xca\
+\x00\x00\x01^\x00\x00\x00\x00\x00\x01\x00\x00\x0d\xca\
+\x00\x00\x01\x88\xe6\xcby\xd4\
+\x00\x00\x00\x10\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
+\x00\x00\x01\x88\xe7\x7f]\xa6\
+\x00\x00\x01\x92\x00\x00\x00\x00\x00\x01\x00\x00\x10v\
+\x00\x00\x01\x88\xe6\xceK0\
+\x00\x00\x00\xe6\x00\x00\x00\x00\x00\x01\x00\x00\x09|\
+\x00\x00\x01\x88\xe7~\xd6\xe7\
+\x00\x00\x00\xb0\x00\x00\x00\x00\x00\x01\x00\x00\x061\
+\x00\x00\x01\x88\xe6\xcd\x7f\xb8\
+\x00\x00\x02\x00\x00\x00\x00\x00\x00\x01\x00\x00\x15\x8b\
+\x00\x00\x01\x88\xe7\x83\x98\xe2\
+\x00\x00\x01\x10\x00\x00\x00\x00\x00\x01\x00\x00\x0a\xe5\
+\x00\x00\x01\x88\xe7\x84\x87 \
+\x00\x00\x00\x80\x00\x00\x00\x00\x00\x01\x00\x00\x04X\
+\x00\x00\x01\x88\xe77\x88\x80\
+\x00\x00\x00<\x00\x00\x00\x00\x00\x01\x00\x00\x01`\
+\x00\x00\x01\x88\xe7|\x8e&\
+\x00\x00\x016\x00\x00\x00\x00\x00\x01\x00\x00\x0c\x5c\
+\x00\x00\x01\x88\xe7\x88\x0e\x9e\
+\x00\x00\x01\xb4\x00\x00\x00\x00\x00\x01\x00\x00\x12\x89\
+\x00\x00\x01\x88\xe7{A\xe9\
+"
+
+def qInitResources():
+    QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+def qCleanupResources():
+    QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+qInitResources()
